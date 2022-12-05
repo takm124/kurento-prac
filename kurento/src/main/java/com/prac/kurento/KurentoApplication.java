22a060bc2a82ef8a6a70e614439c507eab22cd46
Seongho-Shin <sim2308@naver.com>
2022-12-05 14:17:12 +0900

    project init

---
+++ kurento/src/main/java/com/prac/kurento/KurentoApplication.java
@@ -0,0 +1,13 @@
+package com.prac.kurento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KurentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KurentoApplication.class, args);
+	}
+
+}
